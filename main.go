da84442da6be41a478fa1b7b0866e2949e3d0420
Robert Brignull <robertbrignull@gmail.com>
2019-12-22 18:36:11 +0000

    add args parsing

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "github.com/robertbrignull/adventofcode2018/day1"
+import (
+	"github.com/robertbrignull/adventofcode2018/day1"
+	"log"
+	"os"
+	"strconv"
+)
+
+func runDay(dayNum int) {
+	switch dayNum {
+	case 1: day1.Run()
+	}
+}
 
 func main() {
-	day1.Run()
+	args := os.Args
+
+	if len(args) == 1 {
+		for i := 0; i < 25; i++ {
+			runDay(i)
+		}
+	} else if len(args) == 2 {
+		dayNum, err := strconv.Atoi(args[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		runDay(dayNum)
+	} else {
+		log.Fatal("Usage: go run main.go [day-num]")
+	}
 }
